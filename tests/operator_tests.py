a4a2c7915a334e03d04d719266ae67b75b8fe45e
Nico Schlömer <nico.schloemer@gmail.com>
2011-12-24 19:42:01 +0100

    separated mesh and operator tests

--- tests/operator_tests.py
+++ tests/operator_tests.py
@@ -111,54 +111,6 @@ class TestJacobian(unittest.TestCase):
                           -0.00422214252093753 ]
         self._run_test(filename, mu, actual_values )
 # ==============================================================================
-class TestMesh(unittest.TestCase):
-    # --------------------------------------------------------------------------
-    def setUp(self):
-        return
-    # --------------------------------------------------------------------------
-    def _run_test(self, filename, actual_values ):
-        # read the mesh
-        mesh, psi, A, field_data = mesh_io.read_mesh( filename )
-
-        # build the model evaluator
-        mu = 0.0
-        self.ginla_modeleval = \
-            ginla_modelevaluator.GinlaModelEvaluator(mesh, A, mu)
-
-        # Compute the control volumes.
-        self.ginla_modeleval._compute_control_volumes()
-
-        tol = 1.0e-12
-
-        # Get their norms
-        norm = np.linalg.norm( self.ginla_modeleval.control_volumes, ord=1 )
-        self.assertAlmostEqual( actual_values[0], norm, delta=tol )
-        norm = np.linalg.norm( self.ginla_modeleval.control_volumes, ord=2 )
-        self.assertAlmostEqual( actual_values[1], norm, delta=tol )
-        norm = np.linalg.norm( self.ginla_modeleval.control_volumes, ord=np.Inf)
-        self.assertAlmostEqual( actual_values[2], norm, delta=tol )
-    # --------------------------------------------------------------------------
-    def test_rectanglesmall(self):
-        filename = 'rectanglesmall.e'
-        actual_values = [ 10.0,
-                          5.0,
-                          2.5 ]
-        self._run_test(filename, actual_values )
-    # --------------------------------------------------------------------------
-    def test_pacman(self):
-        filename = 'pacman.e'
-        actual_values = [ 302.52270072101,
-                          15.3857579093391,
-                          1.12779746704366 ]
-        self._run_test(filename, actual_values )
-    # --------------------------------------------------------------------------
-    def test_cubesmall(self):
-        filename = 'cubesmall.e'
-        actual_values = [ 10.0,
-                          3.53553390593274,
-                          1.25 ]
-        self._run_test(filename, actual_values)
-# ==============================================================================
 if __name__ == '__main__':
     unittest.main()
 # ==============================================================================
