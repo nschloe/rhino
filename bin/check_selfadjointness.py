5df38a22f64ac0a8bb4ce1cacd8bf4848bc9212c
Nico Schlömer <nico.schloemer@gmail.com>
2012-02-02 15:11:45 +0100

    check_selfadjointness.py: make state random, too

---
+++ bin/check_selfadjointness.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==============================================================================
+import numpy as np
+
+import mesh.mesh_io
+import pyginla.ginla_modelevaluator
+#import pyginla.numerical_methods as nm
+# ==============================================================================
+def _main():
+    '''Main function.
+    '''
+    args = _parse_input_arguments()
+
+    pyginlamesh, psi, A, field_data = mesh.mesh_io.read_mesh(args.filename,
+                                                             timestep=args.timestep
+                                                             )
+    # build the model evaluator
+    mu = 1.0e-1
+    ginla_modeleval = \
+        pyginla.ginla_modelevaluator.GinlaModelEvaluator(pyginlamesh, A, mu)
+
+    N = len( pyginlamesh.nodes )
+    current_psi = np.random.rand(N,1) + 1j * np.random.rand(N,1)
+
+    print 'machine eps = %g' % np.finfo(np.complex).eps
+    
+    # check the jacobian operator
+    J = ginla_modeleval.get_jacobian(current_psi)
+    print 'max(|<v,Ju> - <Jv,u>|) = %g' % _check_selfadjointness(J, ginla_modeleval.inner_product)
+
+    # check the preconditioner
+    P = ginla_modeleval.get_preconditioner(current_psi)
+    print 'max(|<v,Pu> - <Pv,u>|) = %g' % _check_selfadjointness(P, ginla_modeleval.inner_product)
+
+    return
+# ==============================================================================
+def _check_selfadjointness( operator, inner_product ):
+    N = operator.shape[0]
+    num_samples = 1000
+    max_discrepancy = 0.0
+    for k in xrange(num_samples):
+        u = np.random.rand(N,1) + 1j * np.random.rand(N,1)
+        v = np.random.rand(N,1) + 1j * np.random.rand(N,1)
+        alpha = inner_product(v, operator*u)[0,0]
+        beta = inner_product(operator*v, u)[0,0]
+        max_discrepancy = max(max_discrepancy, abs(alpha-beta))
+
+    return max_discrepancy
+# ==============================================================================
+def _parse_input_arguments():
+    '''Parse input arguments.
+    '''
+    import argparse
+
+    parser = argparse.ArgumentParser( description = 'Solve the linearized Ginzburg--Landau problem.'
+                                    )
+
+    parser.add_argument( 'filename',
+                         metavar = 'FILE',
+                         type    = str,
+                         help    = 'ExodusII file containing the geometry and initial state'
+                       )
+
+    parser.add_argument( '--timestep', '-t',
+                         metavar='TIMESTEP',
+                         dest='timestep',
+                         nargs='?',
+                         type=int,
+                         const=0,
+                         default=0,
+                         help='read a particular time step (default: 0)'
+                       )
+
+    args = parser.parse_args()
+
+    return args
+# ==============================================================================
+if __name__ == "__main__":
+    _main()
+# ==============================================================================
