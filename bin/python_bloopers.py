41a69309d2b505d605bdc668e08a6a27abdbfeed
André Gaul <gaul@web-yard.de>
2012-02-02 14:35:20 +0100

    add comment for python blooper

--- bin/python_bloopers.py
+++ bin/python_bloopers.py
@@ -2,9 +2,11 @@
 import numpy as np
 # ==============================================================================
 def _main():
-    a = np.ones(3)
+    a = np.ones((1,3))
     b = np.ones((2,1))
     print a - b # ???
+    # note by Andre: this is somewhat 'more' consistent than MATLAB!
+    # In MATLAB you can do ones(1,3)+1.0 but not ones(1,3)+ones(2,1).
 
     #a = np.array([[1,2], [3,4]])
     #x = np.array([[1],[1]])
