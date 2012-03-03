376998a3d738671723308c1dfd57dfd1b6cd8392
Nico Schlömer <nico.schloemer@gmail.com>
2012-03-04 00:18:09 +0100

    extend main_newton()'s functionality to play the search droid for interesting states

--- bin/main_newton.py
+++ bin/main_newton.py
@@ -10,28 +10,67 @@ import pyginla.ginla_modelevaluator as gm
 import voropy
 # ==============================================================================
 def _main():
-    '''Main function.
-    '''
     filename = _parse_input_arguments()
 
-    #import warnings
-    #warnings.warn('fff')
     # read the mesh
-    print "Reading the mesh...",
+    print 'Reading the mesh...',
     mesh, point_data, field_data = voropy.read( filename )
     #point_data['psi'] = point_data['psi'][:,0] \
                       #+ 1j * point_data['psi'][:,1]
-    print "done."
+    print 'done.'
 
     # build the model evaluator
-    mu = 1.0
+    mu = 0.5
     ginla_modelval = gm.GinlaModelEvaluator(mesh, point_data['A'], mu)
 
     # initial guess
     num_nodes = len(mesh.node_coords)
-    psi0 = 0.5 * np.ones((num_nodes,1), dtype=complex)
+    psi0 = 1.0 * np.ones((num_nodes,1), dtype=complex)
+    newton_out = newton(ginla_modelval, psi0)
+    # write the solution to a file
+    ginla_modelval.mesh.write('solution.e', {'psi': newton_out['x']})
+    # energy of the state
+    print 'Energy of the final state: %g.' % ginla_modelval.energy( newton_out['x'] )
 
-    print "Performing Newton iteration..."
+    #find_sweet_state( ginla_modelval )
+    return
+# ==============================================================================
+def find_sweet_state( ginla_modelval ):
+    '''Loop through a set of parameters/initial states and try to find
+    starting points that (quickly) lead to "interesting looking" solutions.
+    Such solutions are filtered out only by their energy at the moment.'''
+
+    # define search space:
+    Mu = np.linspace(0.0, 1.0, 11)
+    scalePsi0 = np.linspace(0.0, 1.0, 11)
+
+    # initial guess
+    num_nodes = len(ginla_modelval.mesh.node_coords)
+
+    k = 0
+    for mu in Mu:
+        ginla_modelval.set_parameter(mu)
+        for alpha in scalePsi0:
+            print 'mu = %g, alpha = %g' % (mu, alpha)
+            psi0 = alpha * np.ones((num_nodes,1), dtype=complex)
+            newton_out = newton(ginla_modelval, psi0, debug=False)
+            if newton_out['info'] == 0:
+                energy = ginla_modelval.energy( newton_out['x'] )
+                if energy > -0.9 and energy < -0.1:
+                    # store the file
+                    filename = 'sol' + repr(k).rjust(3,'0') + '.vtu'
+                    print 'Interesting state found at mu=%g, scaling=%g. Storing in %s...' \
+                        % (mu, alpha, filename)
+                    ginla_modelval.mesh.write(filename, {'psi': newton_out['x']})
+                    k += 1
+
+    return
+# ==============================================================================
+def newton(ginla_modelval, psi0, debug=True):
+    '''Solve with Newton.
+    '''
+
+    print 'Performing Newton iteration...'
     # perform newton iteration
     newton_out = nm.newton(psi0,
                            ginla_modelval,
@@ -44,10 +83,10 @@ def _main():
                            use_preconditioner = True,
                            deflate_ix = False,
                            num_deflation_vectors = 0,
-                           debug=True,
-                           newton_maxiter = 25
+                           debug=debug,
+                           newton_maxiter = 7
                            )
-    print " done."
+    print ' done.'
     print newton_out['Newton residuals']
     #assert( newton_out['info'] == 0 )
 
@@ -57,10 +96,7 @@ def _main():
     #pp.show()
     #matplotlib2tikz.save('w-defl.tex')
 
-    # energy of the state
-    print "Energy of the final state: %g." % ginla_modelval.energy( newton_out['x'] )
-
-    #print "Performing Poor man's continuation..."
+    #print 'Performing Poor man's continuation...'
     #nm.poor_mans_continuation( psi0,
                                #ginla_modelval,
                                #mu,
@@ -68,12 +104,7 @@ def _main():
                                #nonlinear_tol = 1.0e-8
                              #)
 
-    # write the solution to a file
-    mesh.write('solution.e',
-               {'psi': newton_out['x']}
-               )
-
-    return
+    return newton_out
 # ==============================================================================
 def multiplot_data_series( list_of_data_vectors ):
     '''Plot a list of data vectors with increasing black value.'''
@@ -102,7 +133,7 @@ def _parse_input_arguments():
     
     return args.filename
 # ==============================================================================
-if __name__ == "__main__":
+if __name__ == '__main__':
     _main()
 
     #import cProfile
