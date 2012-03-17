0008f9e2f88302c5566dd3fe6735517ab8346788
Nico Schlömer <nico.schloemer@gmail.com>
2012-03-17 22:47:13 +0100

    find_beautiful_states(): reset solution memory for each new problem. This prevents memory problems.

--- bin/find_beautiful_states.py
+++ bin/find_beautiful_states.py
@@ -38,54 +38,60 @@ def find_beautiful_states( ginla_modeleval ):
     # initial guess
     num_nodes = len(ginla_modeleval.mesh.node_coords)
 
-    interesting_solutions = []
 
     solution_id = 0
     psi0 = np.empty((num_nodes,1), dtype=complex)
-    for mu, alpha, kx, ky in ((a,b,c,d) for a in Mu for b in Alpha for c in Frequencies for d in Frequencies):
-        ginla_modeleval.set_parameter(mu)
-        print 'mu = %g, alpha = %g, kx = %g, ky = %g' % (mu, alpha, kx, ky)
-        # Set the intitial guess for Newton.
-        #psi0 = alpha * np.ones((num_nodes,1), dtype=complex)
-        for i, node in enumerate(ginla_modeleval.mesh.node_coords):
-            psi0[i] = alpha * np.cos(kx * node[0]) * np.cos(ky * node[1])
-        newton_out = newton(ginla_modeleval, psi0, debug=False)
-        print 'Num MINRES iterations:', [len(resvec) for resvec in newton_out['linear relresvecs']]
-        print 'Newton residuals:', newton_out['Newton residuals']
-        if newton_out['info'] == 0:
-            num_newton_iters = len(newton_out['linear relresvecs'])
-            psi = newton_out['x']
-            # Use the energy as a measure for ruling out boring states
-            # such as psi==0 or psi==1 overall.
-            energy = ginla_modeleval.energy( psi )
-            print 'Energy of solution state: %g.' % energy
-            if energy > -0.999 and energy < -0.001:
-                # Store the file as VTU such that ParaView can loop through
-                # and display them at once.
-                filename = 'interesting-newton' + repr(num_newton_iters).rjust(2,'0') \
-                         + '-id' + repr(solution_id).rjust(3,'0') \
-                         + '.vtu'
-                print 'Interesting!',
-                # Check if we already stored that one.
-                already_found = False
-                for state in interesting_solutions:
-                    # Synchronize the complex argument.
-                    state *= np.exp(1j * (np.angle(psi[0]) - np.angle(state[0])))
-                    diff = psi - state
-                    if ginla_modeleval.inner_product(diff, diff) < 1.0e-10:
-                        already_found = True
-                        break
-                if already_found:
-                    print '-- But we already have that one.'
-                else:
-                    interesting_solutions.append(psi)
-                    print 'Storing in %s.' % filename
-                    ginla_modeleval.mesh.write(filename,
-                                               point_data={'psi': psi, 'A': ginla_modeleval._raw_magnetic_vector_potential},
-                                               field_data={'mu': mu, 'alpha': alpha, 'kx': kx, 'ky': ky}
-                                               )
-                    solution_id += 1
-        print
+    # Loop over problem parameters.
+    for mu in Mu:
+        # Reset the solutions each time the problem parameters change.
+        found_solutions = []
+        # Loop over initial states.
+        for alpha, kx, ky in ((a,b,c) for a in Alpha for b in Frequencies for c in Frequencies):
+            ginla_modeleval.set_parameter(mu)
+            print 'mu = %g, alpha = %g, kx = %g, ky = %g' % (mu, alpha, kx, ky)
+            # Set the intitial guess for Newton.
+            #psi0 = alpha * np.ones((num_nodes,1), dtype=complex)
+            for i, node in enumerate(ginla_modeleval.mesh.node_coords):
+                psi0[i] = alpha * np.cos(kx * node[0]) * np.cos(ky * node[1])
+            newton_out = newton(ginla_modeleval, psi0, debug=False)
+            print 'Num MINRES iterations:', [len(resvec) for resvec in newton_out['linear relresvecs']]
+            print 'Newton residuals:', newton_out['Newton residuals']
+            if newton_out['info'] == 0:
+                num_newton_iters = len(newton_out['linear relresvecs'])
+                psi = newton_out['x']
+                # Use the energy as a measure for ruling out boring states
+                # such as psi==0 or psi==1 overall.
+                energy = ginla_modeleval.energy( psi )
+                print 'Energy of solution state: %g.' % energy
+                if energy > -0.999 and energy < -0.001:
+                    # Store the file as VTU such that ParaView can loop through
+                    # and display them at once. For this, also be sure to keep
+                    # the file name in the format 'sdgfds-01414.vtu'.
+                    filename = 'interesting-' \
+                             + repr(num_newton_iters).rjust(2,'0') \
+                             + repr(solution_id).rjust(3,'0') \
+                             + '.vtu'
+                    print 'Interesting!',
+                    # Check if we already stored that one.
+                    already_found = False
+                    for state in found_solutions:
+                        # Synchronize the complex argument.
+                        state *= np.exp(1j * (np.angle(psi[0]) - np.angle(state[0])))
+                        diff = psi - state
+                        if ginla_modeleval.inner_product(diff, diff) < 1.0e-10:
+                            already_found = True
+                            break
+                    if already_found:
+                        print '-- But we already have that one.'
+                    else:
+                        found_solutions.append(psi)
+                        print 'Storing in %s.' % filename
+                        ginla_modeleval.mesh.write(filename,
+                                                  point_data={'psi': psi, 'A': ginla_modeleval._raw_magnetic_vector_potential},
+                                                  field_data={'mu': mu, 'alpha': alpha, 'kx': kx, 'ky': ky}
+                                                  )
+                        solution_id += 1
+            print
 
     return
 # ==============================================================================
