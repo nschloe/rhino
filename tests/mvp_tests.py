b72b0a39c906c12731eb932ece4e0f57af42e186
Nico Schlömer <nico.schloemer@gmail.com>
2012-04-26 13:14:10 +0200

    added test case for magnetic vector potentials

---
+++ tests/mvp_tests.py
@@ -0,0 +1,88 @@
+import voropy
+import pyginla.magnetic_vector_potentials as mvp
+import numpy as np
+import unittest
+from scipy.sparse import spdiags
+# ==============================================================================
+class TestF(unittest.TestCase):
+    # --------------------------------------------------------------------------
+    def setUp(self):
+        return
+    # --------------------------------------------------------------------------
+    def _run_test(self, filename, control_values):
+        '''Test $\int_{\Omega} A^2$.'''
+
+        # read the mesh
+        mesh, point_data, field_data = voropy.read( filename )
+        if mesh.control_volumes is None:
+            mesh.compute_control_volumes()
+
+        tol = 1.0e-13
+
+        A = mvp.constant_z(mesh.node_coords)
+        integral = np.sum(mesh.control_volumes * np.sum(A**2, axis=1))
+        self.assertAlmostEqual(control_values['z'], integral, delta = tol)
+
+        # If this is a 2D mesh, append the z-component 0 to each node
+        # to make sure that the magnetic vector potentials can be
+        # calculated.
+        points = mesh.node_coords.copy()
+        if points.shape[1] == 2:
+            points = np.column_stack((points, np.zeros(len(points))))
+        
+        A = mvp.magnetic_dipole(points,
+                                x0 = np.array([0,0,10]),
+                                m = np.array([0,0,1])
+                                )
+        integral = np.sum(mesh.control_volumes * np.sum(A**2, axis=1))
+        self.assertAlmostEqual(control_values['dipole'], integral, delta = tol)
+        
+
+        #A = mvp.magnetic_dot(mesh.node_coords, radius=2.0, heights=[10, 11])
+        A = np.empty((len(points), 3), dtype=float)
+        for k, node in enumerate(points):
+            A[k] = mvp.magnetic_dot(node[0], node[1], 2.0, 10.0, 11.0)
+        integral = np.sum(mesh.control_volumes * np.sum(A**2, axis=1))
+        self.assertAlmostEqual(control_values['dot'], integral, delta = tol)
+
+        return
+    # --------------------------------------------------------------------------
+    def test_rectanglesmall(self):
+        filename = 'rectanglesmall.e'
+        control_values = {'z': 63.125,
+                          'dipole': 0.00012850741240854054,
+                          'dot': 0.015062114210353949
+                          }
+        self._run_test(filename, control_values)
+        return
+    # --------------------------------------------------------------------------
+    def test_pacman(self):
+        filename = 'pacman.e'
+        control_values = {'z': 3730.2268660993054,
+                          'dipole': 0.0037630906971841487,
+                          'dot': 0.46680833312236825
+                          }
+        self._run_test(filename, control_values)
+        return
+    # --------------------------------------------------------------------------
+    def test_cubesmall(self):
+        filename = 'cubesmall.e'
+        control_values = {'z': 1.25,
+                          'dipole': 0.00015098959555300608,
+                          'dot': 0.00052724018725507451
+                          }
+        self._run_test(filename, control_values)
+        return
+    # --------------------------------------------------------------------------
+    def test_brick(self):
+        filename = 'brick-w-hole.e'
+        control_values = {'z': 1687.6928071551067,
+                          'dipole': 0.014339810567783946,
+                          'dot': 0.42750907995762516
+                          }
+        self._run_test(filename, control_values)
+        return
+# ==============================================================================
+if __name__ == '__main__':
+    unittest.main()
+# ==============================================================================
