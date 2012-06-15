cf4684ff61c96c45ca783731cfcdf6dd6ae9423d
André Gaul <gaul@web-yard.de>
2012-06-15 14:50:55 +0200

    multiple newton files

--- bin/visualize_efficiency.py
+++ bin/visualize_efficiency.py
@@ -17,25 +17,24 @@ def _main():
     args = _parse_input_arguments()
 
     # read the files
-    newton_data = yaml.load_all(open(args.newton_data))
-    newtond_data = yaml.load_all(open(args.newtond_data))
     proj_timings = yaml.load(open(args.proj_timings))
     minres_timing = yaml.load(open(args.minres_timing))
 
-
-    vanilla_newton = list(newton_data)[0]
-    assert vanilla_newton['ix deflation'] == False
-    assert vanilla_newton['extra deflation'] == 0
-    num_vanilla_minres_steps = len(vanilla_newton['Newton results'][25]['relresvec']) - 1
+    #vanilla_newton = list(newton_data)[0]
+    #assert vanilla_newton['ix deflation'] == False
+    #assert vanilla_newton['extra deflation'] == 0
+    #num_vanilla_minres_steps = len(vanilla_newton['Newton results'][25]['relresvec']) - 1
+    num_vanilla_minres_steps = args.num_vanilla_minres_steps
     time_vanilla_minres_step = min(minres_timing['tests'][0]['timings']) / minres_timing['number']
 
-    for newton_data_set in [list(newton_data), list(newtond_data)]:
-        print 111
+    for newton_data_file in args.newton_data_files:
+        newton_data = yaml.load_all(open(newton_data_file))
         x = []
         y = []
-        for newton_datum in list(newton_data):
+        for newton_datum in newton_data:
             if len(newton_datum['Newton results']) >= 25:
                 num_steps = len(newton_datum['Newton results'][25]['relresvec']) - 1
+                print(num_steps)
 
                 # Get projection time.
                 num_defl_vecs = newton_datum['extra deflation']
@@ -58,7 +57,7 @@ def _main():
         #pp.plot(x, y, '-o', color='0.6')
 
     pp.ylim([0, 1])
-    pp.title(vanilla_newton['filename'])
+    pp.title('bla')
 
     # Write the info out to files.
     if args.imgfile:
@@ -75,18 +74,25 @@ def _parse_input_arguments():
 
     parser = argparse.ArgumentParser( description = 'Visualize Newton output.' )
 
-    parser.add_argument('--newton-data','-n',
-                        metavar = 'FILE',
+#    parser.add_argument('--newton-data','-n',
+#                        metavar = 'FILE',
+#                        required = True,
+#                        type    = str,
+#                        help    = 'File containing vanilla Newton data (without ix-deflation)'
+#                        )
+
+    parser.add_argument('--num-vanilla-minres-steps','-v',
                         required = True,
-                        type    = str,
-                        help    = 'File containing vanilla Newton data (without ix-deflation)'
+                        type    = int,
+                        help    = 'number of vanilla minres steps'
                         )
 
-    parser.add_argument('--newtond-data','-d',
+    parser.add_argument('--newton-data-files','-d',
                         metavar = 'FILE',
+                        nargs = '+',
                         required = True,
                         type    = str,
-                        help    = 'File containing Newton data with ix-deflation'
+                        help    = 'Files containing Newton data with ix-deflation'
                         )
 
     parser.add_argument('--proj-timings','-p',
