ef601128017cb737e738267c34f4881a5455ea97
Nico Schlömer <nico.schloemer@gmail.com>
2012-06-21 18:12:39 +0200

    add visualize_newton_residuals.py

--- bin/visualize_newton_residuals.py
+++ bin/visualize_newton_residuals.py
@@ -21,21 +21,23 @@ def _main():
     data = yaml.load(handle)
     handle.close()
 
-    # Plot relresvecsi.
-    #pp.subplot(121)
+    # Plot Newton residuals.
     # Mind that the last Newton datum only contains the final ||F||.
-    num_newton_steps = len(data['Newton results']) - 1
+    num_newton_steps = len(data['Newton results'])
+    x = range(num_newton_steps)
+    # Extract Newton residuals
+    y = np.empty(num_newton_steps)
     for k in xrange(num_newton_steps):
-        pp.semilogy(data['Newton results'][k]['relresvec'], color=str(1.0 - float(k+1)/num_newton_steps))
-    pp.xlabel('Krylov step')
-    pp.ylabel('||r||/||b||')
+        y[k] = data['Newton results'][k]['Fx_norm']
+    # Plot it.
+    pp.semilogy(x, y)
+
+    pp.xlabel('Newton step')
+    pp.ylabel('||F||')
     pp.title('Krylov: %s    Prec: %r    ix-defl: %r    extra defl: %r    ExpRes: %r    Newton iters: %d' %
              (data['krylov'], data['preconditioner type'], data['ix deflation'],
               data['extra deflation'], data['explicit residual'], num_newton_steps)
              )
-    if args.xmax:
-        pp.xlim( [0, args.xmax] )
-    pp.ylim( [1e-10, 10] )
 
     # Write the info out to files.
     if args.imgfile:
@@ -50,18 +52,12 @@ def _parse_input_arguments():
     '''
     import argparse
 
-    parser = argparse.ArgumentParser( description = 'Visualize Newton output.' )
+    parser = argparse.ArgumentParser( description = 'Visualize Newton residuals.' )
 
     parser.add_argument('filename',
                         metavar = 'FILE',
                         type    = str,
                         help    = 'Newton data file'
-                        )
-
-    parser.add_argument('--xmax', '-x',
-                        type    = int,
-                        default = None,
-                        help    = 'maximum number of iterations'
                         )
 
     parser.add_argument('--imgfile', '-i',
