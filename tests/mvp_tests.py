7c9798edd515bebc1ab287610e3bdcaf70c9d6bb
Nico Schlömer <nico.schloemer@gmail.com>
2014-03-12 16:31:24 +0100

    pep8 in tests

--- tests/mvp_tests.py
+++ tests/mvp_tests.py
@@ -2,26 +2,26 @@ import voropy
 import pynosh.magnetic_vector_potentials as mvp
 import numpy as np
 import unittest
-from scipy.sparse import spdiags
-# ==============================================================================
+
+
 class TestF(unittest.TestCase):
-    # --------------------------------------------------------------------------
+
     def setUp(self):
         return
-    # --------------------------------------------------------------------------
+
     def _run_test(self, filename, control_values):
         '''Test $\int_{\Omega} A^2$.'''
 
         # read the mesh
-        mesh, point_data, field_data = voropy.read( filename )
+        mesh, point_data, field_data = voropy.read(filename)
         if mesh.control_volumes is None:
             mesh.compute_control_volumes()
 
         tol = 1.0e-13
 
-        A = mvp.constant_field(mesh.node_coords, np.array([0,0,1]))
+        A = mvp.constant_field(mesh.node_coords, np.array([0, 0, 1]))
         integral = np.sum(mesh.control_volumes * np.sum(A**2, axis=1))
-        self.assertAlmostEqual(control_values['z'], integral, delta = tol)
+        self.assertAlmostEqual(control_values['z'], integral, delta=tol)
 
         # If this is a 2D mesh, append the z-component 0 to each node
         # to make sure that the magnetic vector potentials can be
@@ -31,11 +31,11 @@ class TestF(unittest.TestCase):
             points = np.column_stack((points, np.zeros(len(points))))
 
         A = mvp.magnetic_dipole(points,
-                                x0 = np.array([0,0,10]),
-                                m = np.array([0,0,1])
+                                x0=np.array([0, 0, 10]),
+                                m=np.array([0, 0, 1])
                                 )
         integral = np.sum(mesh.control_volumes * np.sum(A**2, axis=1))
-        self.assertAlmostEqual(control_values['dipole'], integral, delta = tol)
+        self.assertAlmostEqual(control_values['dipole'], integral, delta=tol)
 
         #import time
         #start = time.time()
@@ -46,10 +46,9 @@ class TestF(unittest.TestCase):
         #end = time.time()
         #print end-start
         integral = np.sum(mesh.control_volumes * np.sum(A**2, axis=1))
-        self.assertAlmostEqual(control_values['dot'], integral, delta = tol)
-
+        self.assertAlmostEqual(control_values['dot'], integral, delta=tol)
         return
-    # --------------------------------------------------------------------------
+
     def test_rectanglesmall(self):
         filename = 'rectanglesmall.e'
         control_values = {'z': 63.125,
@@ -58,7 +57,7 @@ class TestF(unittest.TestCase):
                           }
         self._run_test(filename, control_values)
         return
-    # --------------------------------------------------------------------------
+
     def test_pacman(self):
         filename = 'pacman.e'
         control_values = {'z': 3730.2268660993054,
@@ -67,7 +66,7 @@ class TestF(unittest.TestCase):
                           }
         self._run_test(filename, control_values)
         return
-    # --------------------------------------------------------------------------
+
     def test_cubesmall(self):
         filename = 'cubesmall.e'
         control_values = {'z': 1.25,
@@ -76,7 +75,7 @@ class TestF(unittest.TestCase):
                           }
         self._run_test(filename, control_values)
         return
-    # --------------------------------------------------------------------------
+
     def test_brick(self):
         filename = 'brick-w-hole.e'
         control_values = {'z': 1687.6928071551067,
@@ -85,7 +84,6 @@ class TestF(unittest.TestCase):
                           }
         self._run_test(filename, control_values)
         return
-# ==============================================================================
+
 if __name__ == '__main__':
     unittest.main()
-# ==============================================================================
